7505ad0d04d2158853a8b3da939ec499a7484f3e
palindrom8 <andr.nitgrafika@yandex.ru>
2024-07-24 15:37:09 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+# импорты библиотек
+import main
+from graphlern import Pixel, Message
+
+
+# объявление переменных(констант)
+GRID_WIDTH = 10
+GRID_HEIGHT = 10
+PIXEL_SIZE = 50
+BACKGROUND_COLOR = "#ffeba1"
+SOUND = ""
+
+ 
+
+# отрисовка
+def draw(window):
+    # Здесь пиши свой код
+
+    # Пример рисования пикселя с помощью команды Pixel
+    Pixel(window, x=6, y=7, color="green", size=PIXEL_SIZE)
+
+
+            
